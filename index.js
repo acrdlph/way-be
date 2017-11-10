59b6026c72c0426eb7f34a4fe56fb8043f90bcd6
Vimukthi <vbwickramasinghe@gmail.com>
2017-11-10 20:10:43 +0000

    enable CORS

--- index.js
+++ index.js
@@ -27,6 +27,11 @@ app.param('user_id', function (req, res, next, user_id) {
   req.user_id = user_id || 'user_id';
   return next();
 });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
