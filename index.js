3aabd8b4ebf481857b20c9d44d9d93cde0ddfdfe
Vimukthi <vbwickramasinghe@gmail.com>
2017-11-10 17:31:11 +0000

    POST /users/new done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const UserModel = require('./models/user');
 const MessageModel = require('./models/message');
 const co = require('co');
 const moment = require('moment');
-
+const bodyParser = require('body-parser');
 
 //Set up default mongoose connection
 let mongoDB = 'mongodb://waitlist:waitlist@ds257245.mlab.com:57245/waitlist';
@@ -20,6 +20,8 @@ let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
@@ -36,7 +38,7 @@ app.get('/users', (req, res) => co(function *() {
                                    .diff(new Date(), 'minutes')
                    }
               });
-       return res.json(users);
+       res.json(users);
     })
     .catch(err => {
         console.info(err);
@@ -44,31 +46,53 @@ app.get('/users', (req, res) => co(function *() {
     })
 );
 
-app.post('/users', (req, res) => co(function *() {
-       users = yield UserModel.find({});
-
-       return res.json(users);
+app.post('/users/new', (req, res) => co(function *() {
+       let location = req.body.location;
+       let waiting_time = req.body.waiting_time;
+       let new_user = new UserModel(
+           {
+                waiting_time: waiting_time,
+                location: location,
+                created_at: new Date()
+           });
+       yield new_user.save();
+       res.send({
+            id: new_user.id,
+            waiting_time: new_user.waiting_time,
+            location: new_user.location,
+            created_at: new_user.created_at
+       });
     })
     .catch(err => {
-        log.info(err);
+        console.info(err);
+        res.send({error: err});
+    })
+);
+
+app.post('/users/update', (req, res) => co(function *() {
+       let location = req.body.location;
+       res.send(location);
+    })
+    .catch(err => {
+        console.info(err);
         res.send({error: err});
     })
 );
 
 app.get('/users/create', (req, res) => {
 // TODO special endpoint to create users
-//    let awesome_instance = new UserModel(
-//    { name: 'awesome',
-//    interests: 'nothing',
-//    waiting_time: 60,
-//    location: "Amsterdam",
-//      created_at: new Date()});
-//
-//    // Save the new model instance, passing a callback
-//    awesome_instance.save(function (err) {
-//      if (err) return handleError(err);
-//      // saved!
-//    });
+    let awesome_instance = new UserModel(
+    { name: 'awesome',
+    interests: 'nothing',
+    waiting_time: 60,
+    location: "Amsterdam",
+      created_at: new Date()});
+
+    // Save the new model instance, passing a callback
+    awesome_instance.save(function (err) {
+      if (err) return handleError(err);
+      // saved!
+    });
 });
 
 app.listen(3001, () => console.log('Example app listening on port 3001!'));
