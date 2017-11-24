a68b21d710366d545262671b882b5b6298d18db7
Vimukthi <vbwickramasinghe@gmail.com>
2017-11-25 00:39:05 +0100

    Added logging, config

--- app/logger.js
+++ app/logger.js
@@ -5,7 +5,7 @@ const logger = bunyan.createLogger({
 });
 
 logger.logRequest = (req) => {
-    logger.info('request received for %s with ', req.path, {query: req.query, params: req.params});
+    logger.info('request received for %s %s with ', req.method, req.path, {query: req.query, params: req.params});
 }
 
 module.exports = logger;
