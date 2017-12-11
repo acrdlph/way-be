b18c44d92f0a746d744fb6e93242a948b8390019
ulfbiallas <ulfbiallas@googlemail.com>
2017-12-11 23:32:35 +0100

    add endpoint to save feedback

---
+++ app/feedback.js
@@ -0,0 +1,19 @@
+const co = require('co');
+const util = require('./util');
+const feedback_model = require('./models/feedback');
+
+exports.save = function* (req, res) {
+    const {email, feedback} = req.body;
+    if (!feedback) {
+        throw util.createError(400, "Please provide proper feedback!");
+    }
+
+    const new_feedback = new feedback_model({
+        email,
+        feedback,
+        created_at: new Date()
+    });
+
+    yield new_feedback.save();
+    res.json(new_feedback);
+}
