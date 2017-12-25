3d14361b1900115e2a4a564bf2a72d78aa9668f3
Vimukthi <vbwickramasinghe@gmail.com>
2017-12-25 15:02:49 +0100

    Test framework setup correctly with more tests

---
+++ test/test_helper.js
@@ -0,0 +1,27 @@
+const expect = require('chai').expect;
+
+exports.delay = function delay(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    });
+}
+
+exports.tryWithSuccess = function* (promise) {
+    try {
+        const res = yield promise;
+        return res;
+    } catch (err) {
+        expect(true).to.equal(false); // make the test fail
+    }
+}
+
+exports.tryWithFailure = function* (promise, exception_name) {
+    try {
+        yield promise;
+        expect(true).to.equal(false); // make the test fail
+    } catch (err) {
+        expect(err.name).to.equal(exception_name);
+    }
+}
