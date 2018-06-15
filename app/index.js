992f7bc2135f21f09f57a02e6cbc33bcd8b697c5
acrdlph <achill@way.network>
2018-06-15 14:41:11 +0200

    changed contract

--- app/index.js
+++ app/index.js
@@ -148,7 +148,7 @@ if (typeof web3 !== 'undefined') {
   // set the provider you want from Web3.providers
   web3 = new Web3(new Web3.providers.HttpProvider('http://ec2-34-245-74-26.eu-west-1.compute.amazonaws.com:8545'))
 }
-const address = '0x56aeab032d7c1b1c00732b501477038b657cce04'
+const address = '0xbaa593e9c1f11bbcfa4725085211d764eec26592'
 const contract = web3.eth.contract(abi).at(address)
 const event = contract.allEvents({fromBlock: 0, toBlock: 'latest'})
 const user_repository = require('./user/user_repository')
